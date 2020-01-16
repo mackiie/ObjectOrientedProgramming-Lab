eb789d6c425da48aa9a50320b13a9d142f64c825
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-01-16 10:58:54 +0530

    New project

--- Expreiment_2/adding_two_numbers/src/com/company/Main.java
+++ Expreiment_2/adding_two_numbers/src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+        //Adding two numbers
     }
 }
