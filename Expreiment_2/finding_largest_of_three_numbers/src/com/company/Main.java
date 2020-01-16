d745d2a12922a8e2b142a5d2c7db12482c9de4f4
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-01-16 10:11:46 +0530

    finding largest of three numbers

--- Expreiment_2/finding_largest_of_three_numbers/src/com/company/Main.java
+++ Expreiment_2/finding_largest_of_three_numbers/src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main
 
     public static void main(String[] args)
     {
-        if(Integer.parseInt(args[0])>Integer.parseInt(args[1]) && Integer.parseInt(args[0])>Integer.parseInt(args[2])
+        if((Integer.parseInt(args[0]) > Integer.parseInt(args[1])) && (Integer.parseInt(args[0]) > Integer.parseInt(args[2])))
         {
             System.out.println(args[0]);
         }
