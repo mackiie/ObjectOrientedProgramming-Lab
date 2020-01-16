b65381fb1e838c2513889904790da53c7dc01143
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-01-16 11:18:05 +0530

    command line calculator

--- Expreiment_2/command_line_calculator/src/com/company/Main.java
+++ Expreiment_2/command_line_calculator/src/com/company/Main.java
@@ -7,22 +7,27 @@ public class Main {
         if(args[1].contentEquals("+"))
         {
             System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[2]));
+            // Adding two numbers
         }
         else if(args[1].contentEquals("-"))
         {
             System.out.println(Integer.parseInt(args[0]) - Integer.parseInt(args[2]));
+            // Subtracting two numbers
         }
         else if(args[1].contentEquals("*"))
         {
             System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[2]));
+            // Multiplying two numbers
         }
         else if(args[1].contentEquals("/"))
         {
             System.out.println(Integer.parseInt(args[0]) / Integer.parseInt(args[2]));
+            // Deviding two numbers
         }
         else if(args[1].contentEquals("%"))
         {
             System.out.println(Integer.parseInt(args[0]) % Integer.parseInt(args[2]));
+            // Modulus of two numbers
         }
         else
         {
