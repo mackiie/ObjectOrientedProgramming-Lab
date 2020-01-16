659b3198d31d23c8e6df1eb51c4a1632e178225e
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-01-16 10:47:00 +0530

    fibonacci series

--- Expreiment_2/printing_fibonacci/src/com/company/Main.java
+++ Expreiment_2/printing_fibonacci/src/com/company/Main.java
@@ -7,12 +7,16 @@ public class Main
     {
         int a = 0;
         int b = 1;
+        // Initialised values
         int temp;
+        // Temporary variable
         int iterate = 0;
         System.out.println(a);
+        //Printing first value
 
         for(iterate = 0;iterate < 10;iterate++)
         {
+          //Printing further values
             System.out.println(b);
             temp = a;
             a = b;
