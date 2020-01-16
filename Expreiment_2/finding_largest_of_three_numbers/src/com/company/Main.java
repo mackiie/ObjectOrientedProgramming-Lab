406e24fce135f83cc2972e82357973a1824d2853
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-01-16 10:32:19 +0530

    finding max of three numbers

--- Expreiment_2/finding_largest_of_three_numbers/src/com/company/Main.java
+++ Expreiment_2/finding_largest_of_three_numbers/src/com/company/Main.java
@@ -5,16 +5,20 @@ public class Main
 
     public static void main(String[] args)
     {
+      //Program to calculate maximum of three numbers using Command Line Arguments
         if((Integer.parseInt(args[0]) > Integer.parseInt(args[1])) && (Integer.parseInt(args[0]) > Integer.parseInt(args[2])))
         {
+          //Conditions to check first argument
             System.out.println(args[0]);
         }
         else if(Integer.parseInt(args[1]) > Integer.parseInt(args[2]))
         {
+          //Condition to check second argument
             System.out.println(args[1]);
         }
         else
         {
+          //Condition to check third argument
             System.out.println(args[2]);
         }
     }
