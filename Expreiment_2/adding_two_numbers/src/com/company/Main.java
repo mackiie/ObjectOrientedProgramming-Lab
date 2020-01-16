22c62ee5ca022f1230dc15e047693bc6832a89d6
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-01-16 10:18:16 +0530

    sum of two numbers

--- Expreiment_2/adding_two_numbers/src/com/company/Main.java
+++ Expreiment_2/adding_two_numbers/src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main
 
     public static void main(String[] args)
     {
-	// write your code here
+        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
     }
 }
