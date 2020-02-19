fccbedb87c5419bbbd69bafa2a8196a464a404e8
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-02-19 18:32:58 +0530

    marks of ten students

--- Expreiment_2/marks_of_ten_students/src/com/company/Main.java
+++ Expreiment_2/marks_of_ten_students/src/com/company/Main.java
@@ -1,10 +1,45 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
-	// write your code here
+        int marks[];
+        marks = new int[10];
+
+        Scanner s = new Scanner(System.in);
+
+        for(int i=0;i<10;i++)
+        {
+            System.out.print("Enter marks of student " + (i+1) + " : ");
+            marks[i] = s.nextInt();
+        }
+
+        Arrays.sort(marks);
+
+        for(int i=0;i<10;i++)
+        {
+            if(marks[i]>=40 && marks[i]<=50)
+            {
+                System.out.println("PASS");
+            }
+            else if (marks[i]>=51 && marks[i]<=75)
+            {
+                System.out.println("MERIT");
+            }
+            else if (marks[i]>75)
+            {
+                System.out.println("DISTINCTION");
+            }
+            else
+            {
+                System.out.println("not defined");
+            }
+        }
+
     }
 }
