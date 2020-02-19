efd6678654c0361569938ed73474769f8010bb88
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-02-19 20:00:06 +0530

    sum of squares

--- Expreiment_2/sum_of_squares/src/dot/questionmark/Main.java
+++ Expreiment_2/sum_of_squares/src/dot/questionmark/Main.java
@@ -1,8 +1,27 @@
 package dot.questionmark;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        int numbers[];
+        numbers = new int[10];
+
+        int sum = 0;
+
+        Scanner s = new Scanner(System.in);
+
+        for(int i=0;i<10;i++)
+        {
+            numbers[i] = s.nextInt();
+            System.out.println("Square of " + numbers[i] + " is : " + Math.pow(numbers[i],2));
+            sum += Math.pow(numbers[i],2);
+        }
+
+        System.out.println("Sum of squares is : " + sum);
+
+
     }
 }
