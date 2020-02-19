3d142947f88639ef0c8692a5e0526d175ae45f87
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-02-19 20:23:04 +0530

    sum of integers divisible by 5

--- Expreiment_2/sum_of_integers_divisible_by_5/src/dot/questionmark/Main.java
+++ Expreiment_2/sum_of_integers_divisible_by_5/src/dot/questionmark/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args)
     {
-	// write your code here
+        int result = 0;
+
+        for(int i=40;i<250;i++)
+        {
+            if(i%5==0)
+            {
+                result+=i;
+            }
+        }
+
+        System.out.println(result);
     }
 }
