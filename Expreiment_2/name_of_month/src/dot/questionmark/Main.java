2b9965c20986a33c46eaaad5f86d3fe634611a34
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-02-19 20:00:16 +0530

    name of month

---
+++ Expreiment_2/name_of_month/src/dot/questionmark/Main.java
@@ -0,0 +1,17 @@
+package dot.questionmark;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int month_number;
+
+        Scanner s = new Scanner(System.in);
+
+        month_number = s.nextInt();
+
+        
+    }
+}
