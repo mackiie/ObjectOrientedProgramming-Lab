7692c5fb3a090a5d98810ca02f2e32a66c785121
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-02-20 10:49:33 +0530

    interface and class

--- Experiment_5/interface_and_class/src/dot/questionmark/Main.java
+++ Experiment_5/interface_and_class/src/dot/questionmark/Main.java
@@ -3,7 +3,7 @@ package dot.questionmark;
 interface A
 {
     int meth1();
-    int meth2()
+    int meth2();
 }
 
 class MyClass implements A
@@ -34,7 +34,7 @@ public class Main
     public static void main(String[] args)
     {
         MyClass c = new MyClass(7,2);
-        
+
         System.out.println(c.meth1());
         System.out.println(c.meth2());
     }
