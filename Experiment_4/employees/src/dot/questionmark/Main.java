ef1909dffb36c4213f5e15f9d67e2fc0bf6489d8
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-02-20 10:09:51 +0530

    employees

--- Experiment_4/employees/src/dot/questionmark/Main.java
+++ Experiment_4/employees/src/dot/questionmark/Main.java
@@ -1,10 +1,55 @@
 package dot.questionmark;
 
+class employee
+{
+    String name;
+    int emp_id;
+    int salary;
+
+    employee(String n, int e, int s)
+    {
+        name = n;
+        emp_id = e;
+        salary = s;
+    }
+
+    void print()
+    {
+        System.out.println("Name : " + name);
+        System.out.println("Salary : " + salary);
+    }
+
+    void increase(int percent)
+    {
+        salary += salary*percent/100;
+    }
+}
+
+class manager extends employee
+{
+    String department;
+
+    manager(String name, int e, int s, String d)
+    {
+        super(name, e, s);
+        department = d;
+    }
+
+    void show()
+    {
+        super.print();
+        System.out.println("Department : " + department);
+    }
+}
+
 public class Main
 {
 
     public static void main(String[] args)
     {
-	// write your code here
+        employee e = new employee("ABC",54174, 50000);
+        e.print();
+        manager m = new manager("XYZ",11111,100000,"reception");
+        m.show();
     }
 }
