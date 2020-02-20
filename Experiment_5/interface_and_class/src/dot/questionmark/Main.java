44db16ea82730e78aad0395bc2476f0d6404921e
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-02-20 10:48:58 +0530

    interface and class

---
+++ Experiment_5/interface_and_class/src/dot/questionmark/Main.java
@@ -0,0 +1,41 @@
+package dot.questionmark;
+
+interface A
+{
+    int meth1();
+    int meth2()
+}
+
+class MyClass implements A
+{
+    int num1;
+    int num2;
+
+    MyClass(int n1, int n2)
+    {
+        num1 = n1;
+        num2 = n2;
+    }
+
+    public int meth1()
+    {
+        return num1 - num2;
+    }
+
+    public int meth2()
+    {
+        return num1 + num2;
+    }
+}
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        MyClass c = new MyClass(7,2);
+        
+        System.out.println(c.meth1());
+        System.out.println(c.meth2());
+    }
+}
