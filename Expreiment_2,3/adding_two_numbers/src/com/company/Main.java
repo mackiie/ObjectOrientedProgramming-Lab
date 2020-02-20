df2a1cf5a19a60da3c943e0a4bff8060facf5281
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-02-20 10:24:11 +0530

    adding two numbers

---
+++ Expreiment_2,3/adding_two_numbers/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main
+{
+  // Arguments passed : 2 3
+
+    public static void main(String[] args)
+    {
+        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+        //Adding two numbers
+    }
+}
