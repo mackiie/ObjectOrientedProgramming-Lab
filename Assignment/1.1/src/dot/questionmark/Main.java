0a6a4a3c9085ef97f804afdc14dbeedf6a814ebf
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-03-05 09:52:12 +0530

    Assignment 1

--- Assignment/1.1/src/dot/questionmark/Main.java
+++ Assignment/1.1/src/dot/questionmark/Main.java
@@ -23,7 +23,12 @@ abstract class Animal
         return age;
     }
 
-    abstract void talk();
+    void talk()
+    {
+        System.out.println("Name of the animal : " + name);
+        System.out.println("Age of the animal : " + age);
+        System.out.println("Type of the animal : " + animalType);
+    }
 }
 
 class Tiger extends Animal
@@ -42,6 +47,26 @@ class Tiger extends Animal
         System.out.println("Hello, I am a " + animalType + " and my name is " + getName() + ".");
         System.out.println("I am " + getAge() + " years old.");
         System.out.println("My weight is " + weight + " kg.");
+        System.out.println("-----------------------------------------------------------------------------");
+    }
+}
+
+class Lion extends Animal
+{
+    String nickName;
+
+    Lion(String name,int age, String animalType, String nickName)
+    {
+        super(name, age, animalType);
+        this.nickName = nickName;
+    }
+
+    @Override
+    void talk()
+    {
+        System.out.println("Hello, I am a " + animalType + " and my name is " + getName() + ".");
+        System.out.println("I am " + getAge() + " years old.");
+        System.out.println("My nickname is " + nickName);
         System.out.println("-----------------------------------------------------------------------------");
     }
 }
