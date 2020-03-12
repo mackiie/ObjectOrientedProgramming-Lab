7da98a90e590412b0807fe10095ec5da9a56bb26
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-03-12 10:07:09 +0530

    upper case via console

---
+++ Experiment_8/console/src/dot/questionmark/Main.java
@@ -0,0 +1,14 @@
+package dot.questionmark;
+
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string");
+        System.out.println(sc.nextLine().toUpperCase());
+    }
+}
